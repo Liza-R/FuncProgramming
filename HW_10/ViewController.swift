5228adf0501ee1060b25a42ef64276276eac9655
Liza-R <83958561+Liza-R@users.noreply.github.com>
2021-10-01 18:53:33 +0300

    Adding map func

--- HW_10/ViewController.swift
+++ HW_10/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         oneSorted()
+        twoMap()
     }
     func oneSorted(){
-        print("Сортировка массива:")
+        print("Сортировка массива")
         var massInt: [Int] = []
         for _ in 0...19{
             let n = Int.random(in: 20...40)
@@ -22,6 +23,16 @@ class ViewController: UIViewController {
         }
         print("Массив с рандомными числами:\n\(massInt)\n")
         print("Отсортированный массив:\n\(massInt.sorted())")
+    }
+    func twoMap(){
+        print("Перевод чисел в строки")
+        var massInt: [Int] = []
+        for _ in 0...19{
+            let n = Int.random(in: 20...40)
+            massInt.append(n)
+        }
+        print("Массив с рандомными числами:\n\(massInt)\n")
+        print("Отформатированный массив:\n\(massInt.map {$0.description})")
     }
 }
 
